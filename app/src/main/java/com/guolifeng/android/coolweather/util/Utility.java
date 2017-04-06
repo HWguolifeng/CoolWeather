acc979ddc580f3d42b1564cd6065e12e55aa9eae
guolifeng <glf_work@163.com>
2017-04-07 00:07:33 +0800

    7：最终项目的完成

--- app/src/main/java/com/guolifeng/android/coolweather/util/Utility.java
+++ app/src/main/java/com/guolifeng/android/coolweather/util/Utility.java
@@ -2,9 +2,11 @@ package com.guolifeng.android.coolweather.util;
 
 import android.text.TextUtils;
 
+import com.google.gson.Gson;
 import com.guolifeng.android.coolweather.db.City;
 import com.guolifeng.android.coolweather.db.County;
 import com.guolifeng.android.coolweather.db.Province;
+import com.guolifeng.android.coolweather.gson.Weather;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -100,5 +102,21 @@ public class Utility
         return false;
     }
 
-
+    /**
+     * 将返回的JSON数据解析成Weather实体类
+     */
+    public static Weather handleWeatherResponse(String response)
+    {
+        try
+        {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent, Weather.class);
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
